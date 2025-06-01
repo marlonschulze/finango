e4f59276df5842bde4070e4a505ce8fa8318c006
marlonschulze <marlonschulze2804@gmail.com>
2025-06-01 08:30:43 -0300

    Primeira Versão

---
+++ js/script.js
@@ -0,0 +1,37 @@
+  localStorage.setItem('user', 'admin');
+  localStorage.setItem('email', 'admin123@gmail.com');
+  localStorage.setItem('password', 'admin123')
+  
+  const btnLogin = document.getElementById('btn-login');
+  btnLogin.addEventListener('click', function(event){
+  event.preventDefault();
+
+  const user = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  const userCorrect = localStorage.getItem('user');
+  const emailCorrect = localStorage.getItem('email');
+  const passwordCorrect = localStorage.getItem('password');
+
+  if(user === userCorrect && email === emailCorrect && password === passwordCorrect){
+    window.location.href = 'html/finango.html';
+  } else{
+      alert(`Login attempt failed. 
+        Try again!`);
+  }
+})
+
+/*password visibility*/ 
+function passwordVisibility(){
+  const passwordNotVisible = document.getElementById('password');
+  const eyeIcon = document.getElementById('togglePassword');
+
+  if(passwordNotVisible.type === "password"){
+    passwordNotVisible.type = "text";
+    eyeIcon.innerHTML = "🕶️";
+  } else{
+    passwordNotVisible.type = "password";
+    eyeIcon.innerHTML = "👁"
+  }
+}
